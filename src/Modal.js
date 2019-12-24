4170ea9775383d6f783af4a8fb595489ca308451
tsybenko <tsybenko.dmitry@gmail.com>
2019-12-25 01:09:37 +0200

    FIX: window object passed into module function

--- src/Modal.js
+++ src/Modal.js
@@ -1,4 +1,4 @@
-export default (function(el, triggers = [], options = {}) {
+export default (window => function(el, triggers = [], options = {}) {
 
 	this.initedHook = function(cb) {cb()};
 	// this.triggerAddedHook = function(cb) {cb()};
