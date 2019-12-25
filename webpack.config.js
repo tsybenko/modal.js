0866b45df3d4ad783688538bfad3e46bd3c488a3
tsybenko <tsybenko.dmitry@gmail.com>
2019-12-25 19:17:53 +0200

    * "npm run build" to "npm run build-dev" and "npm run build-prod"

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,28 @@
 const path = require('path');
+const MODE_PROD = 'production';
+const MODE_DEV = 'development';
 
-module.exports = {
-	entry: './src/index.js',
-	output: {
-		filename: 'bundle.js',
-		path: path.resolve(__dirname, 'dist'),
-		library: "Modal"
-	},
-	mode: "development",
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				loader: "babel-loader"
-			}
-		],
+module.exports = (env) => {
+	let mode = env.production === true
+		? MODE_PROD
+		: MODE_DEV;
+
+	return {
+		entry: './src/index.js',
+		output: {
+			filename: 'bundle.js',
+			path: path.resolve(__dirname, 'dist'),
+			library: "Modal"
+		},
+		mode,
+		module: {
+			rules: [
+				{
+					test: /\.js$/,
+					exclude: /node_modules/,
+					loader: "babel-loader"
+				}
+			],
+		}
 	}
 };
