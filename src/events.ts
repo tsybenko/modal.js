9571328bd054e47364c9d09a4f4c7c2168c1eb33
tsybenko <tsybenko.dmitry@gmail.com>
2020-01-12 00:28:36 +0200

    * Added bubbles into an event object options and reformatted code

--- src/events.ts
+++ src/events.ts
@@ -10,12 +10,41 @@ import {
 import { SystemEvent } from './defaults/system-event';
 
 export default () => ({
-	[BEFORE_OPEN]: (data = SystemEvent): CustomEvent => new CustomEvent(BEFORE_OPEN, { detail: data }),
-	[ON_OPEN]: (data = SystemEvent): CustomEvent => new CustomEvent(ON_OPEN, { detail: data, cancelable: true }),
-	[OPENED]: (data = SystemEvent): CustomEvent => new CustomEvent(OPENED, { detail: data }),
-	[BEFORE_CLOSE]: (data = SystemEvent): CustomEvent => new CustomEvent(BEFORE_CLOSE, { detail: data }),
-	[ON_CLOSE]: (data = SystemEvent): CustomEvent => new CustomEvent(ON_CLOSE, { detail: data, cancelable: true }),
-	[CLOSED]: (data = SystemEvent): CustomEvent => new CustomEvent(CLOSED, { detail: data }),
+	[BEFORE_OPEN]: (data = SystemEvent): CustomEvent =>
+		new CustomEvent(BEFORE_OPEN, {
+			detail: data,
+			bubbles: true
+		}),
+	[ON_OPEN]: (data = SystemEvent): CustomEvent =>
+		new CustomEvent(ON_OPEN, {
+			detail: data,
+			cancelable: true,
+			bubbles: true
+		}),
+	[OPENED]: (data = SystemEvent): CustomEvent =>
+		new CustomEvent(OPENED, {
+			detail: data,
+			bubbles: true
+		}),
+	[BEFORE_CLOSE]: (data = SystemEvent): CustomEvent =>
+		new CustomEvent(BEFORE_CLOSE, {
+			detail: data,
+			bubbles: true
+		}),
+	[ON_CLOSE]: (data = SystemEvent): CustomEvent =>
+		new CustomEvent(ON_CLOSE, {
+			detail: data,
+			cancelable: true,
+			bubbles: true
+		}),
+	[CLOSED]: (data = SystemEvent): CustomEvent =>
+		new CustomEvent(CLOSED, {
+			detail: data,
+			bubbles: true
+		}),
 	/** Special events */
-	[SYSTEM_DEFAULT_EVENT]: (data = SystemEvent): CustomEvent => new CustomEvent(SYSTEM_DEFAULT_EVENT),
+	[SYSTEM_DEFAULT_EVENT]: (): CustomEvent =>
+		new CustomEvent(SYSTEM_DEFAULT_EVENT, {
+			bubbles: true
+		}),
 });
