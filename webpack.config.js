c6ec273bd42a1dceedd55b2d32f23ab08eeca012
Dmytro Tsybenko <tsybenko.dmitry@gmail.com>
2026-06-18 19:02:25 +0300

    Migrate and adjust Webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,34 @@
-const path = require('path');
-const MODE_PROD = 'production';
-const MODE_DEV = 'development';
-
-const devServer = require('./devserver.config');
+const path = require("path");
+const MODE_PROD = "production";
+const MODE_DEV = "development";
 
 module.exports = (env) => {
-	let mode = env.production === 'true'
-		? MODE_PROD
-		: MODE_DEV;
+  let mode = env.APP_ENV === "production" ? MODE_PROD : MODE_DEV;
 
-	return {
-		entry: './src/index.ts',
-		output: {
-			filename: 'bundle.js',
-			path: path.resolve(__dirname, 'dist'),
-			library: "Modal"
-		},
-		mode,
-		resolve: {
-			extensions: [ '.ts', '.js' ],
-		},
-		devServer,
-		module: {
-			rules: [
-				{
-					test: /\.ts(x?)$/,
-					exclude: /node_modules/,
-					loaders: [
-						"babel-loader",
-						"ts-loader"
-					],
-				},
-				{
-					test: /\.js$/,
-					exclude: /node_modules/,
-					loader: "babel-loader"
-				}
-			],
-		}
-	}
+  return {
+    entry: "./src/index.ts",
+    output: {
+      filename: "bundle.js",
+      path: path.resolve(__dirname, "dist"),
+      library: "Modal",
+    },
+    mode,
+    resolve: {
+      extensions: [".ts", ".js"],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts(x?)$/,
+          use: ["babel-loader", "ts-loader"],
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.js$/,
+          use: "babel-loader",
+          exclude: /node_modules/,
+        },
+      ],
+    },
+  };
 };
